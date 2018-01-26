4eafd7363ff4426c389c6da7a3b29daedcaac711
[906011748@qq.com] <906011748@qq.com>
2018-01-26 16:56:45 +0800

    add babel

---
+++ public/javascripts/a.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//exports.str = "a";
+// exports = function fn (){
+//     console.log('console b log');
+// };
+
+class MyClass {
+    constructor() {
+        console.log('console b log');
+    }
+}
+
+export default MyClass;
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,12 @@
-(function () {
 
-})();
+'use strict';
+
+//var A = require('./a');
+//var a = new A();
+//console.log(a);
+//console.log(a.fn());
+
+
+import A from './a';
+
+var a = new A();
